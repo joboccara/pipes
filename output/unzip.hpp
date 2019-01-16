6b0ac8535fef60df6da745ae2a5580c5d5d9ffd2
Jonathan Boccara <boccaraj@gmail.com>
2019-01-16 21:59:31 +0100

    Implement unzip in terms of transform

--- output/unzip.hpp
+++ output/unzip.hpp
@@ -1,57 +1,41 @@
 #ifndef output_unzip_h
 #define output_unzip_h
 
+#include "../helpers/FWD.hpp"
 #include "../helpers/meta.hpp"
+#include "transform.hpp"
+
+#include <tuple>
+#include <utility>
 
 namespace fluent
 {
-
-template<typename... Iterators>
-class output_unzip_iterator
-{
-public:
-    using iterator_category = std::output_iterator_tag;
-    using value_type = void;
-    using difference_type = void;
-    using pointer = void;
-    using reference = void;
-    
-    explicit output_unzip_iterator(Iterators... iterators) : iterators_(std::make_tuple(iterators...)) {}
-    output_unzip_iterator& operator++()
-    {
-        detail::apply([](auto&& iterator){ ++iterator; }, iterators_);
-        return *this;
-    }
-    output_unzip_iterator& operator++(int){ ++*this; return *this; }
-    output_unzip_iterator& operator*(){ return *this; }
-    template<typename... Ts>
-    output_unzip_iterator& operator=(std::tuple<Ts...> const& values)
-    {
-        detail::apply2([](auto&& value, auto&& iterator){ *iterator = value; }, values, iterators_);
-        return *this;
-    }
-    template<typename First, typename Second>
-    output_unzip_iterator& operator=(std::pair<First, Second> const& values)
-    {
-        *std::get<0>(iterators_) = values.first;
-        *std::get<1>(iterators_) = values.second;
-        return *this;
-    }
-private:
-    std::tuple<Iterators...> iterators_;
-};
-    
 namespace output
 {
 
-template<typename... Iterators>
-output_unzip_iterator<Iterators...> unzip(Iterators... iterators)
+template <size_t I>
+struct Get
 {
-    return output_unzip_iterator<Iterators...>(iterators...);
+    template <typename Tuple>
+    decltype(auto) operator()(Tuple&& tuple)
+    {
+        return std::get<I>(FWD(tuple));
+    }
+};
+    
+template<size_t... Is>
+auto make_transform(std::index_sequence<Is...> const&)
+{
+    return fluent::output::transform(Get<Is>{}...);
+}
+    
+template<typename... Iterators>
+auto unzip(Iterators... iterators)
+{
+    return make_transform(std::make_index_sequence<sizeof...(Iterators)>{})(iterators...);
 }
     
 } // namespace output
-
 } // namespace fluent
 
 #endif /* output_unzipper_h */
