42585222f53f98e487f391c98d3dfa9a578ec4cc
Jonathan Boccara <boccaraj@gmail.com>
2019-03-03 10:47:31 +0100

    Use a variadic pack of lambdas and index_sequence_for to simplify the code of unzip

--- output/unzip.hpp
+++ output/unzip.hpp
@@ -13,26 +13,16 @@ namespace fluent
 namespace output
 {
 
-template <size_t I>
-struct Get
-{
-    template <typename Tuple>
-    decltype(auto) operator()(Tuple&& tuple)
-    {
-        return std::get<I>(FWD(tuple));
-    }
-};
-    
 template<size_t... Is>
 auto make_transform(std::index_sequence<Is...> const&)
 {
-    return fluent::output::transform(Get<Is>{}...);
+    return transform([](auto&& tup){return std::get<Is>(FWD(tup));}...);
 }
-    
+
 template<typename... Iterators>
 auto unzip(Iterators... iterators)
 {
-    return make_transform(std::make_index_sequence<sizeof...(Iterators)>{})(iterators...);
+    return make_transform(std::index_sequence_for<Iterators...>{})(iterators...);
 }
     
 } // namespace output
