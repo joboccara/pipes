801872d554d99d37cbaf9de8803c3834efedcbe2
Jonathan Boccara <boccaraj@gmail.com>
2019-06-04 13:56:39 +0200

    Restore dead_end_iterator.hpp

---
+++ include/dead_end_iterator.hpp
@@ -0,0 +1,29 @@
+#ifndef dead_end_iterator_h
+#define dead_end_iterator_h
+
+#include <iterator>
+
+namespace pipes
+{
+    
+    class dead_end_iterator
+    {
+    public:
+        using iterator_category = std::output_iterator_tag;
+        using value_type = void;
+        using difference_type = void;
+        using pointer = void;
+        using reference = void;
+        
+        dead_end_iterator& operator++() { return *this; }
+        dead_end_iterator& operator++(int) { return *this; }
+        
+        dead_end_iterator& operator*() { return *this; }
+        
+        template<typename T>
+        dead_end_iterator& operator=(T const&) { return *this; }
+    };
+    
+} // namespace fluent
+
+#endif /* dead_end_iterator_h */
