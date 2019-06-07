9c1b98fe04bb71ed3ea2338c2b5feecc864fefbb
Jonathan Boccara <boccaraj@gmail.com>
2019-06-07 05:47:42 +0200

    Remove obsolete tests

--- tests/partition.cpp
+++ tests/partition.cpp
@@ -46,24 +46,3 @@ TEST_CASE("partition's iterator category should be std::output_iterator_tag")
                   "iterator category should be std::output_iterator_tag");
 }
 
-TEST_CASE("partition cannot override existing contents")
-{
-    /* This code should not compile as the output_partition_iterator is plugged on a vector::begin
-    
-    std::vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
-    std::vector<int> expectedEvens = {2, 4, 6, 8, 10};
-    std::vector<int> expectedOdds = {1, 3, 5, 7, 9};
-    
-    auto const isEvenPartition = pipes::partition([](int n){ return n % 2 == 0; });
-    
-    std::vector<int> evens = {0, 0, 0, 0, 0};
-    std::vector<int> odds = {0, 0, 0, 0, 0};
-    
-    std::copy(begin(input), end(input), isEvenPartition(begin(evens), end(odds)));
-    
-    REQUIRE(evens == expectedEvens);
-    REQUIRE(odds == expectedOdds);
-     
-    */
-}
