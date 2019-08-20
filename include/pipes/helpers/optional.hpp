b21e77f0dec30f061476b1ff5a4883c966555851
Jonathan Boccara <boccaraj@gmail.com>
2019-08-20 11:14:12 +0200

    Use aligned_storage as object, not array of 1 element

--- include/pipes/helpers/optional.hpp
+++ include/pipes/helpers/optional.hpp
@@ -14,14 +14,14 @@ namespace detail
     class optional
     {
     public:
-        optional(const T& object) : m_initialized(true) { new (m_object) T(object); };
+        optional(const T& object) : m_initialized(true) { new (&m_object) T(object); };
         optional(nullopt_t) : m_initialized(false) {};
         optional() : m_initialized(false) {};
-        optional(const optional& other) : m_initialized(other.m_initialized) { if (other.m_initialized) new (m_object) T(*other); }
+        optional(const optional& other) : m_initialized(other.m_initialized) { if (other.m_initialized) new (&m_object) T(*other); }
         optional& operator=(const optional& other)
         {
             reset();
-            new (m_object) T(*other);
+            new (&m_object) T(*other);
             m_initialized = other.m_initialized;
             return *this;
         }
@@ -31,18 +31,18 @@ namespace detail
         void emplace(Args&&... args)
         {
             reset();
-            new (m_object) T(std::forward<Args>(args)...);
+            new (&m_object) T(std::forward<Args>(args)...);
             m_initialized = true;
         }
         operator bool() const { return m_initialized; };
-        T& operator*() { return *reinterpret_cast<T*>(m_object); };
-        const T& operator*() const { return *reinterpret_cast<const T*>(m_object); };
+        T& operator*() { return *reinterpret_cast<T*>(&m_object); };
+        const T& operator*() const { return *reinterpret_cast<const T*>(&m_object); };
         T* operator->() { return &**this; };
         const T* operator->() const { return &**this; };
     private:
         void reset() { if (m_initialized) (&**this)->~T(); m_initialized = false; }
     private:
-        std::aligned_storage_t<sizeof(T)> m_object[1];
+        std::aligned_storage_t<sizeof(T)> m_object;
         bool m_initialized;
     };
 
