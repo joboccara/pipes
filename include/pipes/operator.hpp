4daf7019170ce1aae1eeaafe13a8d46ce18fe979
Jonathan Boccara <boccaraj@gmail.com>
2019-08-21 06:40:28 +0200

    Rename the detection idiom types to make them more expressive

--- include/pipes/operator.hpp
+++ include/pipes/operator.hpp
@@ -13,20 +13,25 @@ namespace pipes
 
     namespace detail
     {
-        struct aPipeline{};
+        namespace impl
+        {
+            struct aPipeline{};
+            template<typename Pipe>
+            using pipe_expression = decltype(std::declval<Pipe&>().plug_to_pipeline(aPipeline{}));
+            
+            template<typename Pipe>
+            constexpr bool pipe_expression_detected = detail::is_detected<pipe_expression, Pipe>;
+            
+            template<typename Pipe>
+            using IsAPipe = std::enable_if_t<pipe_expression_detected<Pipe>, bool>;
+        }
+        
         template<typename Pipe>
-        using pipe_expression = decltype(std::declval<Pipe&>().plug_to_pipeline(aPipeline{}));
-
-        template<typename Pipe>
-        constexpr bool pipe_expression_detected = detail::is_detected<pipe_expression, Pipe>;
-
-        template<typename Pipe>
-        using IsPipe = std::enable_if_t<pipe_expression_detected<Pipe>, bool>;
+        using IsAPipe = impl::IsPipe<std::remove_reference_t<Pipe>>;
         
     } //  namespace detail
     
-    template<typename Pipe, typename Pipeline,
-        detail::IsPipe<std::remove_reference_t<Pipe>> = true>
+    template<typename Pipe, typename Pipeline, detail::IsAPipe<Pipe> = true>
     auto operator>>=(Pipe&& pipe, Pipeline&& pipeline)
     {
         return pipe.plug_to_pipeline(pipeline);
@@ -36,31 +41,39 @@ namespace pipes
     
     namespace detail
     {
-        namespace adl
+        namespace impl
         {
-            using std::begin;
-            using std::end;
-            template<typename T>
-            using begin_expression = decltype(begin(std::declval<T&>()));
-            template<typename T>
-            using end_expression = decltype(end(std::declval<T&>()));
+            namespace adl
+            {
+                using std::begin;
+                using std::end;
+                template<typename T>
+                using begin_expression = decltype(begin(std::declval<T&>()));
+                template<typename T>
+                using end_expression = decltype(end(std::declval<T&>()));
+            }
+            template<typename Range>
+            constexpr bool range_expression_detected = detail::is_detected<adl::begin_expression, Range> && detail::is_detected<adl::end_expression, Range>;
+            
+            template<typename Range>
+            using IsARange = std::enable_if_t<range_expression_detected<Range>, bool>;
+            
+            template<typename OutputIterator>
+            using IsAPipeline = std::enable_if_t<std::is_same<typename OutputIterator::iterator_category, std::output_iterator_tag>::value, bool>;
         }
-        template<typename Range>
-        constexpr bool range_expression_detected = detail::is_detected<adl::begin_expression, Range> && detail::is_detected<adl::end_expression, Range>;
         
         template<typename Range>
-        using IsARange = std::enable_if_t<range_expression_detected<Range>, bool>;
+        using IsARange = impl::IsARange<std::remove_reference_t<Range>>;
         
-        template<typename OutputIterator>
-        using IsAnOutputIterator = std::enable_if_t<std::is_same<typename OutputIterator::iterator_category, std::output_iterator_tag>::value, bool>;
+        template<typename Pipeline>
+        using IsAPipeline = impl::IsAPipeline<std::remove_reference_t<Pipeline>>;
+
     } // namespace detail
     
-    template<typename Range, typename OutputIterator,
-    detail::IsARange<std::remove_reference_t<Range>> = true,
-    detail::IsAnOutputIterator<std::remove_reference_t<OutputIterator>> = true>
-    void operator>>=(Range&& range, OutputIterator&& outputIterator)
+    template<typename Range, typename Pipeline, detail::IsARange<Range> = true, detail::IsAPipeline<Pipeline> = true>
+    void operator>>=(Range&& range, Pipeline&& pipeline)
     {
-        std::copy(begin(range), end(range), outputIterator);
+        std::copy(begin(range), end(range), pipeline);
     }
 
 } // namespace pipes
