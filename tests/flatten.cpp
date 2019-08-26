b4127dd4b85fde8674601fabdeaaa06ad3cc83c0
Jonathan Boccara <boccaraj@gmail.com>
2019-08-26 23:33:39 +0200

    Add test for operator= of flatten pipe

--- tests/flatten.cpp
+++ tests/flatten.cpp
@@ -16,3 +16,15 @@ TEST_CASE("flatten pipe")
     
     REQUIRE(expected == results);
 }
+
+TEST_CASE("flatten pipe operator=")
+{
+    std::vector<int> results1, results2;
+    auto pipeline1 = pipes::flatten >>= pipes::push_back(results1);
+    auto pipeline2 = pipes::flatten >>= pipes::push_back(results2);
+    
+    pipeline2 = pipeline1;
+    send(pipeline2, std::vector<int>{1, 2});
+    REQUIRE(results1.size() == 2);
+    REQUIRE(results2.size() == 0);
+}
