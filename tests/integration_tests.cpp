d1047ac3c4addc94257d9826155989b9492e055b
offa <bm-dev@yandex.com>
2019-11-21 18:59:46 +0100

    GCC 5 and 6 errors fixed:
     - tuple 'initializer list would use explicit constructor'
     - begin / end return type deduction 'use of 'auto' before deduction of 'auto''

--- tests/integration_tests.cpp
+++ tests/integration_tests.cpp
@@ -13,24 +13,24 @@
 TEST_CASE("Mix of various pipes")
 {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
+
     std::vector<int> expectedOutput1 = {6, 12, 18};
     std::vector<int> expectedOutput2 = {2, 4};
     std::vector<int> expectedOutput3 = {2, 10};
     std::vector<int> expectedOutput4 = {1, 5, 7};
     std::vector<char> expectedOutput5 = {'A', 'A', 'A'};
-    
+
     std::vector<int> output1;
     std::vector<int> output2;
     std::vector<int> output3;
     std::vector<int> output4;
     std::vector<char> output5;
-    
+
     auto const times2 = pipes::transform([](int i) { return i*2; });
     auto const divideBy2 = pipes::transform([](int i) { return i/2; });
     auto const pairUpWithA = pipes::transform([](int i) { return std::make_pair(i, 'A'); });
-    
-    
+
+
     std::copy(begin(numbers), end(numbers), pipes::switch_(pipes::case_( [](int n){ return n % 3 == 0; } ) >>= times2 >>= pipes::push_back(output1),
                                                            pipes::case_( [](int n){ return n % 2 == 0; } ) >>= divideBy2 >>= pipes::partition([](int n){ return n % 2 == 0; },
                                                                                                                                              pipes::push_back(output2),
@@ -41,7 +41,7 @@ TEST_CASE("Mix of various pipes")
                                                                                                                                             pipes::push_back(output5)
                                                                                                                                            )
                                                                                                                       ));
-    
+
     REQUIRE(output1 == expectedOutput1);
     REQUIRE(output2 == expectedOutput2);
     REQUIRE(output3 == expectedOutput3);
@@ -56,7 +56,7 @@ TEST_CASE("Compatibility with STL algorithms")
     std::vector<int> results;
 
     std::copy(begin(input), end(input), pipes::transform([](int i){ return i * 2; }) >>= pipes::push_back(results));
-    
+
     REQUIRE(expected == results);
 }
 
@@ -64,12 +64,12 @@ TEST_CASE("Transform and filter")
 {
     std::vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> expected = {8, 16, 24, 32, 40};
-    
+
     auto const isEven = [](int n){ return n % 2 == 0; };
     auto const times2 = [](int n){ return n * 2; };
 
     std::vector<int> results;
-    
+
     SECTION("chaining with >>=")
     {
         input >>= pipes::filter(isEven)
@@ -84,14 +84,14 @@ TEST_CASE("transform then filter calls the transform function only once per elem
 {
     auto const input = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     auto const expected = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    
+
     auto results = std::vector<int>{};
     auto output = std::vector<int>{};
-    
+
     input >>= pipes::transform([&results](int i){ results.push_back(i); return i * 2; })
           >>= pipes::filter([](int i){ return i % 3 == 0; })
           >>= pipes::push_back(output);
-    
+
     REQUIRE(results == expected);
 }
 
@@ -99,12 +99,12 @@ TEST_CASE("Sequence of output iterators, no algorithms")
 {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> expected = {4, 8, 12, 16, 20, 24, 28, 32, 36, 40};
-    
+
     auto const times2 = pipes::transform([](int n){ return n * 2; });
     std::vector<int> results;
-    
+
     numbers >>= times2 >>= times2 >>= pipes::push_back(results);
-    
+
     REQUIRE(results == expected);
 }
 
@@ -112,12 +112,12 @@ TEST_CASE("Sequence of output iterators, no algorithms, with pipes")
 {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> expected = {4, 8, 12, 16, 20, 24, 28, 32, 36, 40};
-    
+
     auto const times2 = [](int n){ return n * 2; };
     std::vector<int> results;
-    
+
     numbers >>= pipes::transform(times2) >>= pipes::transform(times2) >>= pipes::push_back(results);
-    
+
     REQUIRE(results == expected);
 }
 
@@ -130,12 +130,12 @@ TEST_CASE("Sequence of input ranges and output iterators, with pipes")
 {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::vector<int> expected = {4, 8, 12, 16, 20, 24, 28, 32, 36, 40};
-    
+
     auto const times2 = [](int n){ return n * 2; };
     std::vector<int> results;
-    
+
     numbers | numbers | numbers >>= pipes::transform(times2) >>= pipes::transform(times2) >>= pipes::push_back(results);
-    
+
     REQUIRE(results == expected);
 }
 
@@ -147,9 +147,18 @@ namespace MyCollectionNamespace
     {
         std::vector<int> data_;
     };
-    
-    auto begin(MyCollection const& myCollection) { return begin(myCollection.data_); }
-    auto end(MyCollection const& myCollection) { return end(myCollection.data_); }
+
+    auto begin(MyCollection const& myCollection)
+    {
+        using std::begin;
+        return begin(myCollection.data_);
+    }
+
+    auto end(MyCollection const& myCollection)
+    {
+        using std::end;
+        return end(myCollection.data_);
+    }
 }
 }
 
@@ -160,7 +169,7 @@ TEST_CASE("Reading from a collection with ADL begin and end")
     auto results = std::vector<int>{};
 
     input >>= pipes::transform([](int i){ return i *2; }) >>= pipes::push_back(results);
-    
+
     REQUIRE(results == expected);
 }
 
@@ -168,19 +177,19 @@ TEST_CASE("Aggregation of pipes into reusable components")
 {
     auto const input = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     auto results = std::vector<int>{};
-    
+
     SECTION("End of pipeline aggregated")
     {
         auto const expected = std::vector<int>{4, 8, 12, 16, 20};
         auto pipeline = pipes::filter([](int i) { return i % 2 == 0; })
                     >>= pipes::transform([](int i){ return i * 2;})
                     >>= pipes::push_back(results);
-        
+
         input >>= pipeline;
-        
+
         REQUIRE(results == expected);
     }
-    
+
     SECTION("Middle of pipeline aggregated")
     {
         SECTION("Composite of simple pipes")
@@ -188,51 +197,51 @@ TEST_CASE("Aggregation of pipes into reusable components")
             auto const expected = std::vector<int>{4, 8, 12, 16, 20};
             auto pipeline = pipes::filter([](int i) { return i % 2 == 0; })
                         >>= pipes::transform([](int i){ return i * 2;});
-            
+
             input >>= pipeline >>= pipes::push_back(results);
-            
+
             REQUIRE(results == expected);
         }
-        
+
         SECTION("Composite + pipe")
         {
             auto const expected = std::vector<int>{8, 16, 24, 32, 40};
 
             auto pipeline = pipes::filter([](int i) { return i % 2 == 0; })
                         >>= pipes::transform([](int i){ return i * 2;});
-            
+
             auto pipeline2 = pipeline >>= pipes::transform([](int i){ return i * 2;});
-            
+
             input >>= pipeline2 >>= pipes::push_back(results);
 
             REQUIRE(results == expected);
         }
-        
+
         SECTION("pipe + composite")
         {
             auto const expected = std::vector<int>{4, 8, 12, 16, 20, 24, 28, 32, 36, 40};
-            
+
             auto pipeline = pipes::filter([](int i) { return i % 2 == 0; })
                         >>= pipes::transform([](int i){ return i * 2;});
-            
+
             auto pipeline2 = pipes::transform([](int i){ return i * 2;}) >>= pipeline;
-            
+
             input >>= pipeline2 >>= pipes::push_back(results);
-            
+
             REQUIRE(results == expected);
         }
-        
+
         SECTION("composite + composite")
         {
             auto const expected = std::vector<int>{8, 16, 24, 32, 40};
-            
+
             auto pipeline = pipes::filter([](int i) { return i % 2 == 0; })
                         >>= pipes::transform([](int i){ return i * 2;});
-            
+
             auto pipeline2 = pipeline >>= pipeline;
-            
+
             input >>= pipeline2 >>= pipes::push_back(results);
-            
+
             REQUIRE(results == expected);
         }
     }
