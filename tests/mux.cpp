51830b799663d21004373108a13026057686b9da
Jonathan Boccara <boccaraj@gmail.com>
2019-12-15 14:13:44 +0100

    Add test case for max with 3 collections

--- tests/mux.cpp
+++ tests/mux.cpp
@@ -103,3 +103,20 @@ TEST_CASE("mux partition")
     REQUIRE(results1 == expected1);
     REQUIRE(results2 == expected2);
 }
+
+TEST_CASE("mux allows to apply a transform to 3 collections")
+{
+    auto const v1 = std::vector<int>{1, 2, 3, 4, 5};
+    auto const v2 = std::vector<int>{10, 20, 30, 40, 50};
+    auto const v3 = std::vector<int>{100, 200, 300, 400, 500};
+
+    auto const expected = std::vector<int>{111, 222, 333, 444, 555};
+    
+    auto results = std::vector<int>{};
+    
+    pipes::mux(v1, v2, v3)
+    >>= pipes::transform([](int a, int b, int c){ return a + b + c;})
+    >>= pipes::push_back(results);
+    
+    REQUIRE(results == expected);
+}
