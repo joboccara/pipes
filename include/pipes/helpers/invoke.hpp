ef7b9086bc21f6dfe058ac68707debbcae64558f
modanashar <45742845+modanashar@users.noreply.github.com>
2021-12-28 02:13:54 +0100

    fixed problem related to compiling with MSVC with language version c++20
    Usage of std::invoke_result instead of std::result_of, as std::result_of was removed from the MSVC's standard library.

--- include/pipes/helpers/invoke.hpp
+++ include/pipes/helpers/invoke.hpp
@@ -8,23 +8,46 @@ namespace pipes
 {
     namespace detail
     {
+#ifdef _MSC_VER
+#if _MSVC_LANG >  201703L
         template<typename Functor, typename... Args>
         typename std::enable_if<
-        std::is_member_pointer<typename std::decay<Functor>::type>::value,
-        typename std::result_of<Functor&&(Args&&...)>::type
+            std::is_member_pointer<typename std::decay<Functor>::type>::value,
+            typename std::invoke_result<Functor && (Args&&...)>::type
         >::type invoke(Functor&& f, Args&&... args)
         {
             return std::mem_fn(f)(std::forward<Args>(args)...);
         }
-        
+
         template<typename Functor, typename... Args>
         typename std::enable_if<
-        !std::is_member_pointer<typename std::decay<Functor>::type>::value,
-        typename std::result_of<Functor&&(Args&&...)>::type
+            !std::is_member_pointer<typename std::decay<Functor>::type>::value,
+            typename std::invoke_result<Functor && (Args&&...)>::type
         >::type invoke(Functor&& f, Args&&... args)
         {
             return std::forward<Functor>(f)(std::forward<Args>(args)...);
         }
+#else
+        template<typename Functor, typename... Args>
+        typename std::enable_if<
+            std::is_member_pointer<typename std::decay<Functor>::type>::value,
+            typename std::result_of<Functor && (Args&&...)>::type
+        >::type invoke(Functor&& f, Args&&... args)
+        {
+            return std::mem_fn(f)(std::forward<Args>(args)...);
+        }
+
+        template<typename Functor, typename... Args>
+        typename std::enable_if<
+            !std::is_member_pointer<typename std::decay<Functor>::type>::value,
+            typename std::result_of<Functor && (Args&&...)>::type
+        >::type invoke(Functor&& f, Args&&... args)
+        {
+            return std::forward<Functor>(f)(std::forward<Args>(args)...);
+        }
+#endif
+#endif
+
     }
 }
 
